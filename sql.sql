07c7c33da5a62b6e47a05a7c873d1677d61d22e0
gdrapic <gorandrapic@gmail.com>
2015-01-28 14:27:46 -0500

    Update sql.sql

--- sql.sql
+++ sql.sql
@@ -12,3 +12,8 @@ CREATE TABLE BPA_JOB_TYPE_EXCLUSION
   	FOREIGN KEY (JOB_TYPE_ID) REFERENCES BPA_JOB_TYPE (JOB_TYPE_ID),
   	FOREIGN KEY (JOB_TYPE_ID_EXCLUED) REFERENCES BPA_JOB_TYPE (JOB_TYPE_ID)
 );
+
+insert into BPA_JOB_TYPE values (0, '-', '-','Represent all types in exclusion table BPA_JOB_TYPE_EXCLUSION');
+
+insert into BPA_JOB_TYPE_EXCLUSION values (1,0,'BAT excludes All job types');
+insert into BPA_JOB_TYPE_EXCLUSION values (4,0,'DEL excludes All job types!');
