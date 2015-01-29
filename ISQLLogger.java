41814deea0b52680b5510e4c96b9812ba34a9ec7
gdrapic <gorandrapic@gmail.com>
2015-01-28 20:10:20 -0500

    Delete ISQLLogger.java

--- ISQLLogger.java
+++
@@ -1,7 +0,0 @@
-package com.bpadomain.persistence.statements;
-
-public interface ISQLLogger {
-	public abstract <T> T beforeStart(SQLBean sqlBean) throws Exception;
-	public abstract <T> T afterFinish(SQLBean sqlBean) throws Exception;
-	public abstract <T> T afterException(SQLBean sqlBean, Throwable e) throws Exception;
-}
